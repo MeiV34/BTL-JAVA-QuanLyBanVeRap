17000aded841937e3d7796952e462ebf9634b03a
BaoCoder288203 <baon6777@gmail.com>
2024-04-24 07:12:23 +0700

    Ve_01

--- BTL_JAVA/src/connectDB/ConnectDB.java
+++ BTL_JAVA/src/connectDB/ConnectDB.java
@@ -16,15 +16,9 @@ public class ConnectDB {
 	public void connect() {
 		try {
 			String url = "jdbc:sqlserver://localhost:1433;databasename=RapBHV;trustServerCertificate=true;encrypt=true";
-<<<<<<< HEAD
 			String user = "sa";
 			String pwd = "123";
-=======
-			String user = "kriscao";
-			String pwd = "07122002";
->>>>>>> e218dff0cf4707aeea752b27be6ebe6d1c198028
 			con = DriverManager.getConnection(url, user, pwd);
-//			JOptionPane.showMessageDialog(null, "Connected!");
 		}catch(SQLException e) {
 			e.printStackTrace();
 			JOptionPane.showMessageDialog(null, "Unconnected!");
